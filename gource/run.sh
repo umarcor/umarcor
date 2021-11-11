c2cd6d2d14b0238683d7366ba4a5e03fde99d612
umarcor <unai.martinezcorral@ehu.eus>
2021-11-12 00:43:58 +0100

    gource: update

--- gource/run.sh
+++ gource/run.sh
@@ -1,24 +1,22 @@
-#!/usr/bin/env bash
-
-if [ "$#" -ne 0 ]; then
-  GOURCE_ARGS="$@"
-else
-  GOURCE_ARGS="--path ${GOURCE_REPO:-.} -${GOURCE_RESOLUTION:-1920x1024} -a ${GOURCE_AUTO_SKIP_SECONDS:-0.25} -s ${GOURCE_SECOND_PER_DAY:-0.25} --title ${GOURCE_TITLE:-Title}"
-fi
-
-echo "$GOURCE_ARGS"
+#!/usr/bin/env sh
 
 gource \
-    --key \
-    -c 1 \
-    --highlight-all-users \
-    --highlight-dirs \
-    --hide bloom,filenames \
-    --hash-seed 9182 \
-    --date-format "%Y / %m / %d [%W]" \
-    --font-colour aaaaaa \
-    --font-size 24 \
-    --caption-size 4 \
-    --colour-images \
-    --output-framerate 60 \
-    $GOURCE_ARGS
+  --key \
+  -c 0.75 \
+  --highlight-users \
+  --highlight-dirs \
+  --hide bloom,filenames \
+  --hash-seed 9182 \
+  --date-format '%Y / %m / %d [%W]' \
+  --font-colour aaaaaa \
+  --font-size 24 \
+  --caption-size 4 \
+  --colour-images \
+  --output-framerate 60 \
+  --dir-name-depth 2 \
+  --frameless \
+  -"${GOURCE_RESOLUTION:-1920x1080}" \
+  -a "${GOURCE_AUTO_SKIP_SECONDS:-0.05}" \
+  -s "${GOURCE_SECONDS_PER_DAY:-0.15}" \
+  --title "${GOURCE_TITLE:-Title}" \
+  $@
